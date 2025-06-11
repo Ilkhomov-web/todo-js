316cb3a261abaf54e82c474132e44e37a789a32d
ilkhomov-web <ilhomovelyor119@gmail.com>
2025-06-11 14:23:34 +0500

    some chnage

--- index.js
+++ index.js
@@ -29,7 +29,9 @@ event.preventDefault()
         taskName: taskName.value,
         taskDescription: taskDescription.value,
         taskImportance: taskImportance.value,
-        taskTime: taskTime.value
+        taskTime: taskTime.value,
+        classValue: 'layout-card',
+        btnGroup: 'flex',
     }
 tasks.push(newTask)
 localStorage.setItem('task', JSON.stringify(tasks))
@@ -51,32 +53,57 @@ function taskDelete(id) {
   localStorage.setItem('task', JSON.stringify(tasks));
   location.reload()
 }
-tasks.map((item) => {
-  layoutCards.innerHTML += `
-    <div class="layout-card" data-id="${item.id}">
-      <p>Id:${item.id}</p>
-      <h3>${item.taskName}</h3>
-      <p class="desc">${item.taskDescription}</p>
-      <p class="status">${item.taskImportance}</p>
-      <p class="time">${item.taskTime}</p>
-      <div class="btnGroup">
-        <button class="comp" onclick="taskComplete('${item.id}')">
-          <span class="material-symbols-outlined"> task_alt </span>
-        </button>
-        <button class="del" onclick="taskDelete('${item.id}')">
-          <span class="material-symbols-outlined">delete</span>
-        </button>
+const layoutCardss = document.getElementById("layout-cards");
+layoutCardss.innerHTML = "";
+
+if (tasks.length === 0) {
+  layoutCards.innerHTML = `<p style="text-align:center; padding:20px;">Hozircha bo'sh</p>`;
+} else {
+  tasks.map((item) => {
+    layoutCards.innerHTML += `
+      <div class="${item.classValue}" data-id="${item.id}">
+        <p>Id: ${item.btnGroup === "none" ? 'Bajarildi' : item.id}</p>
+        <h3>${item.taskName}</h3>
+        <p class="desc">${item.taskDescription}</p>
+        <p class="status">${item.taskImportance}</p>
+        <p class="time">${item.taskTime}</p>
+        <div class="btnGroup" id="btnGroup">
+          <button class="comp" style="${item.btnGroup === 'none' ? 'display:none;' : ''}" onclick="taskComplete('${item.id}', '${item.classValue}')">
+            <span class="material-symbols-outlined">task_alt</span>
+          </button>
+          <button class="del" onclick="taskDelete('${item.id}')">
+            <span class="material-symbols-outlined">delete</span>
+          </button>
+        </div>
       </div>
-    </div>`;
-});
+    `;
+  });
+}
 
 
 //complete task
-function taskComplete(id){
-  const element = document.querySelector(`.layout-card[data-id='${id}']`);
-  if(element){
-    element.classList.add('compCard')
+function taskComplete(id, value) {
+  const element = document.querySelector(`[data-id='${id}']`);
+  if (element) {
+    element.classList.remove('layout-card');
+    element.classList.add('compCard');
   }
-  console.log('task id:', id, element);
-  
+
+  if (element.className === "compCard") {
+    const btnGroup = element.querySelector('.btnGroup');
+    if (btnGroup) {
+      btnGroup.style.display = 'none';
+    }
+  }
+
+  tasks = tasks.map(task => {
+    if (task.id === id) {
+      return { ...task, classValue: 'compCard', btnGroup: 'none' }; 
+    }
+    return task;
+  });
+
+  localStorage.setItem('task', JSON.stringify(tasks));
+  location.reload()
 }
+console.log(tasks);
